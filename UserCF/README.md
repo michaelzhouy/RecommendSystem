150e736009c9ee853ee30af4ffa51ee0c49e7d98
michaelzhouy <15602409303@163.com>
2022-03-18 09:33:14 +0800

    添加传参

---
+++ UserCF/README.md
@@ -0,0 +1,20 @@
+# 基于用户的协同过滤(UserCF)
+
+## 算法原理
+
+[UserCF算法原理](https://www.cnblogs.com/rainbowly/p/12115346.html)
+
+## 数据集
+
+基于MovieLens数据集(显示反馈数据集)
+
+## 运行
+在该目录下
+1. 训练
+```sh
+python3 train.py --mode train
+```
+2. 预测
+```sh
+python3 train.py --mode test
+```
--- UserCF/train.py
+++ UserCF/train.py
@@ -4,13 +4,23 @@
 import pandas as pd
 from UserCF import UserCF
 import joblib
+import argparse
 
 
-data = pd.read_csv('../data/ml-100k/ua.base', sep='\\t', header=None, names=["userId", "movieId", "rating", "timestamp"],engine='python')
-data.drop(columns=["timestamp"], inplace=True)
-clf = UserCF(method='base')
-clf.fit(data)
-joblib.dump(clf, './userCf.m')
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", required=True, choices=["train", "test"])
+parser.add_argument("--method", type=str, default="base", help="method for calculate similarity")
+parser.add_argument("--modelPath", type=str, default="./userCf.m", help="model path")
+a = parser.parse_args()
 
-clf = joblib.load('./userCf.m')
-print(clf.recommendProducts(1, 15, 10))
+
+if __name__ == "__main__":
+    if a.mode == 'train':
+        data = pd.read_csv('../data/ml-100k/ua.base', sep='\\t', header=None, names=["userId", "movieId", "rating", "timestamp"],engine='python')
+        data.drop(columns=["timestamp"], inplace=True)
+        clf = UserCF(method='base')
+        clf.fit(data)
+        joblib.dump(clf, a.modelPath)
+    elif a.mode == 'test':
+        clf = joblib.load('./userCf.m')
+        print(clf.recommendProducts(1, 15, 10))
